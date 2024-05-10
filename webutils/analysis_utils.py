ca911e19bfa4c0f238dd4f0eaca94d23bb8bfe6d
krugeremily <112472730+emilykruger@users.noreply.github.com>
2024-05-10 14:02:26 +0200

    fixed error in tfidf func

--- webutils/analysis_utils.py
+++ webutils/analysis_utils.py
@@ -8,22 +8,22 @@ nltk.download('stopwords')
 
 
 def calculate_tfidf(tokenized_corpus, folder_path):
-    # Prepare documents and file names from the tokenized corpus
+    #prepare documents and file names from tokenized corpus
     documents = [' '.join(doc) for doc in tokenized_corpus]
-    file_names = file_names = [filename for filename in os.listdir(folder_path) if filename.endswith('.txt')]
+    file_names = [filename for filename in os.listdir(folder_path) if filename.endswith('.txt')]
     
-    # Calculate TF-IDF matrix
+    #calculate TF-IDF matrix
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(documents)
     feature_names = vectorizer.get_feature_names_out()
 
-    # Create a list of dictionaries containing filename and top 5 TF-IDF words
+    #create a list of dictionaries containing filename, top 10 TF-IDF words and scores
     result = []
     for i, filename in enumerate(file_names):
         tfidf_scores = list(zip(feature_names, tfidf_matrix[i].toarray().flatten()))
         tfidf_scores.sort(key=lambda x: x[1], reverse=True)
-        top_tfidf_words = [word for word, score in tfidf_scores[:10]]
-        result.append({'Filename': filename, 'Top 10 TF-IDF Words': top_tfidf_words})
+        top_tfidf_words_with_scores = tfidf_scores[:10]
+        result.append({'Filename': filename, 'Top 10 TF-IDF Words': top_tfidf_words_with_scores})
 
     return result
 
